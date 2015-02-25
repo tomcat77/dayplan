5e906f09338bf1e12e9f53a0ff04044e4c92bc5c
tomcat77 <T3mXfPXOIkXv>
2015-02-25 18:02:06 +0300

    Подключен CSS файл, устанавливается текущая дата

--- dayplan-desktop/src/main/java/dayplan/desktop/ui/DayPlanController.java
+++ dayplan-desktop/src/main/java/dayplan/desktop/ui/DayPlanController.java
@@ -1,15 +1,23 @@
 package dayplan.desktop.ui;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.Date;
+import java.util.ResourceBundle;
 
 /**
  * @author vmuravlev
  */
-public class DayPlanController {
+public class DayPlanController implements Initializable {
 
     @FXML
     private Label todayLabel;
 
-
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        todayLabel.setText(String.format("%1$tA, %1$td.%1$tm.%1$tY", new Date()));
+    }
 }
